cb3bfd0c5d00d3224d5f16a09247245ebb97d204
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-12-04 03:41:23 +0530

    minor

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,8 @@
-import React, {useState, useContext, useEffect} from "react";
-import * as PushApi from "@pushprotocol/restapi";
+import React, { useState, useContext, useEffect } from "react";
+// import * as PushApi from "@pushprotocol/restapi";
 import "./Landing.css";
 import Web3Context from "../contexts";
-import { Link, NavLink } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 
 // const fetchNotifs = async () => {
 //   const notifications = await PushApi.user.getFeeds({
@@ -11,17 +10,19 @@ import { Link, NavLink } from "react-router-dom";
 //     env: "staging",
 //   });
 
-  // console.log("Notifications: \n\n", notifications);
+// console.log("Notifications: \n\n", notifications);
 // };
 
 const RegisterButton = () => {
-  const {sellerI,account} = useContext(Web3Context)
+  const { sellerI, account } = useContext(Web3Context);
   // console.log(sellerI);
-  const [isregistered, setisregistered] = useState(sellerI == 0 ?false : true);
+  const [isregistered, setisregistered] = useState(
+    sellerI === 0 ? false : true
+  );
   useEffect(() => {
     // setisregistered(sellerI!=0);
     console.log(sellerI);
-    if(sellerI == 0){
+    if (sellerI === 0) {
       setisregistered(false);
     } else {
       setisregistered(true);
@@ -30,27 +31,38 @@ const RegisterButton = () => {
   // setTimeout(() => {
   //   setis
   // }, 1000);
-  
-  return(
-    <>
-      {
-        isregistered?
-          <div className="dashbuttons">
-            <NavLink className="dashnavs" to={`/Buyer/${account.currentAccount}`}>I am a Buyer</NavLink>
-            <NavLink className="dashnavs" to={`/Seller/${account.currentAccount}`}>I am a Seller</NavLink>
-          </div>
-        :
-          <div className="registerbuttons">
-            <NavLink className="registernav" to={`/register/${account.currentAccount}`} style={{ textDecoration: 'none' }}>Register</NavLink>
-          </div>
 
-      }
+  return (
+    <>
+      {isregistered ? (
+        <div className="dashbuttons">
+          <NavLink className="dashnavs" to={`/Buyer/${account.currentAccount}`}>
+            I am a Buyer
+          </NavLink>
+          <NavLink
+            className="dashnavs"
+            to={`/Seller/${account.currentAccount}`}
+          >
+            I am a Seller
+          </NavLink>
+        </div>
+      ) : (
+        <div className="registerbuttons">
+          <NavLink
+            className="registernav"
+            to={`/register/${account.currentAccount}`}
+            style={{ textDecoration: "none" }}
+          >
+            Register
+          </NavLink>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 const Landing = () => {
-  const {account} = useContext(Web3Context)
+  const { account } = useContext(Web3Context);
   // fetchNotifs();
   return (
     <>
@@ -59,21 +71,29 @@ const Landing = () => {
         <div className="landingpage_left">
           <div className="landingpage_left_text">
             <div className="landingpage_left_text_heading">WARRETH</div>
-            {account.currentAccount==null?
-            <div className="landingpage_left_text_subheading">
-              We have something, we haven't thought yet.
-            </div>
-            :
-              <RegisterButton/>
-            }
+            {account.currentAccount == null ? (
+              <div className="landingpage_left_text_subheading">
+                We have something, we haven't thought yet.
+              </div>
+            ) : (
+              <RegisterButton />
+            )}
           </div>
           <div className="landingpage_left_img">
-            <img className="landingpage_left_img_triangle" alt="" src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1670048162/ETHINDIA/triangle_nwcumv.png"/>
+            <img
+              className="landingpage_left_img_triangle"
+              alt=""
+              src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1670048162/ETHINDIA/triangle_nwcumv.png"
+            />
           </div>
         </div>
         <div className="landingpage_right">
           <div className="landingpage_right_image">
-            <img alt="" className="landingpage_image" src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1670049574/ETHINDIA/landingpageimg_hiavrd.png"/>
+            <img
+              alt=""
+              className="landingpage_image"
+              src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1670049574/ETHINDIA/landingpageimg_hiavrd.png"
+            />
           </div>
         </div>
       </div>
