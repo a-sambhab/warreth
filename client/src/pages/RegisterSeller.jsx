3a56b7841389eb00ccf6cf9b3424c3ae8db5b1b1
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-12-03 15:17:11 +0530

    Added Register UI

--- client/src/pages/RegisterSeller.jsx
+++ client/src/pages/RegisterSeller.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./RegisterSeller.css";
 
 const RegisterSeller = () => {
+  const { add } = useParams();
   return (
-    <div>RegisterSeller</div>
-  )
-}
+    <div className="registercontain">
+      <div className="registerinnercontain">
+        <p className="registertext">Create Profile</p>
+        <p className="registertextsmall">Wallet Address</p>
+        <input className="textbox" type="text" />
+        <p className="registertextsmall2">Seller ID</p>
+        <input className="textbox2" type="text" />
+        <div className="buttontext">
+          <button className="button" type="submit">
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default RegisterSeller
+export default RegisterSeller;
