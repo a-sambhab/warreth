3909d3eba4376689563b0aabb13bb6f7df8ace03
a-sambhab <saisambhav21chaini@gmail.com>
2022-12-03 10:56:22 +0530

    Added Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css';
 
 const Navbar = () => {
+  const [connected, setConnected] = useState(false);
   return (
-    <div className='navcontainer'>Navbar</div>
+    <div className='navcontainer'>
+      <div className='navlogo'>
+        LOGOOOOO
+      </div>
+      {connected?
+        <div className='connected'>
+          <img src='https://res.cloudinary.com/dgy8ybeoy/image/upload/v1670044645/Group_mr6k1t.png'/>
+          <div className='navaddress'>
+            Hey, 0x84151...19655
+          </div>
+        </div>
+      :
+        <div className='connectwallet'>
+          Connect Wallet
+        </div>
+      }
+    </div>
   )
 }
 
