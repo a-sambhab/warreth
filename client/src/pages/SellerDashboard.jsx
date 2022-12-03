7d1b5478b5d711c7256cb73d3035bef6990cd709
a-sambhab <saisambhav21chaini@gmail.com>
2022-12-03 17:19:29 +0530

    Added SellerUI

--- client/src/pages/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard.jsx
@@ -8,13 +8,15 @@ const SellerDashboard = () => {
   const {add} = useParams();
   const {connectWallet, account, Contract, sellerI} = useContext(Web3Context);
   const [nfts, setNfts] = useState([]);
+  const [status, setStatus] = useState(0)
   useEffect(() => {
     getData();
    // console.log(nfts);
   }, [Contract, account, sellerI]);
   const getData = async () => {
+    console.log(sellerI);
     const res = await getSellerNFTs(Contract, sellerI);
-    //console.log(res)
+    console.log(res);
     setNfts(res);
   };
   return (
@@ -23,18 +25,36 @@ const SellerDashboard = () => {
       <div className="buyerinnercontain">
         <div className="buyersidebar">
           <div className="buyersidebar_warranties">
-            <div className="active_userwarranty">Active Warranties</div>
-            <div className="active_userwarranty">Pending Warranties</div>
-            <div className="active_userwarranty">Expired Warranties</div>
+            <div className="active_userwarranty" onClick={()=>{setStatus(2)}}>Active Warranties</div>
+            <div className="active_userwarranty" onClick={()=>{setStatus(0)}}>Pending Warranties</div>
+            <div className="active_userwarranty" onClick={()=>{setStatus(3)}}>Expired Warranties</div>
           </div>
-          <div to={`/createwarranty/${add}`} className='seller_button'>Create Warranty</div>
+          <NavLink to={`/createwarranty/${add}`} className='seller_button'>Create Warranty</NavLink>
         </div>
         <div className="buyermain">
           <div className="buyermain_cards">
+          {
+            nfts.length && nfts
+              .filter((res)=>res.status==status)
+              .map((obj)=>{
+                const {expiry, status, creationTime, productId, buyers, imageURI, tokenId} = obj;
+                return(
+                  <CardBuyerdashboard
+                  expiry = {expiry}
+                  creationTime = {creationTime}
+                  productId = {productId}
+                  buyers = {buyers}
+                  imageURI = {imageURI}
+                  tokenId = {tokenId}
+                />
+                )
+              })
+
+          }
+            {/* <CardBuyerdashboard />
             <CardBuyerdashboard />
             <CardBuyerdashboard />
-            <CardBuyerdashboard />
-            <CardBuyerdashboard />
+            <CardBuyerdashboard /> */}
           </div>
         </div>
       </div>
