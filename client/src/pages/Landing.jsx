a27fbf9de7f7e7f020d95c3e480233e0d7e33f08
kumarshantanu01 <shantanuk280@gmail.com>
2022-12-03 12:12:02 +0530

    Landing Page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -16,16 +16,24 @@ const Landing = () => {
   return (
     <>
       <div className="landingpage">
-      {/* <div className="landingcontain">Landing</div> */}
+        {/* <div className="landingcontain">Landing</div> */}
         <div className="landingpage_left">
           <div className="landingpage_left_text">
             <div className="landingpage_left_text_heading">WARRETH</div>
-            <div className="landingpage_left_text_subheading">We have something, we havennt thought yet.</div>
+            <div className="landingpage_left_text_subheading">
+              We have something, we haven't thought yet.
+            </div>
+          </div>
+          <div className="landingpage_left_img">
+            <img className="landingpage_left_img_triangle" alt="" src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1670048162/ETHINDIA/triangle_nwcumv.png"/>
           </div>
         </div>
-        <div className="landingpage_right"></div>
+        <div className="landingpage_right">
+          <div className="landingpage_right_image">
+            <img alt="" className="landingpage_image" src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1670049574/ETHINDIA/landingpageimg_hiavrd.png"/>
+          </div>
+        </div>
       </div>
-    
     </>
   );
 };
