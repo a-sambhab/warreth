63d8738a3be36c3a5fe8ab7b774567d2e418ea3e
kumarshantanu01 <shantanuk280@gmail.com>
2022-12-03 13:42:05 +0530

    buyer seller dashboard

--- client/src/pages/BuyerDashboard.jsx
+++ client/src/pages/BuyerDashboard.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import './BuyerDashboard.css'
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./BuyerDashboard.css";
+import { CardBuyerdashboard } from "../components/Card_buyerdashboard";
 
 const BuyerDashboard = () => {
-  const {add} = useParams();
+  const { add } = useParams();
   return (
-    <div className='buyercontain'>
-      <div className='buyerinnercontain'>
-        <div className='buyersidebar'>
-
+    <div className="buyercontain">
+      <div className="buyerinnercontain">
+        <div className="buyersidebar">
+          <div className="buyersidebar_warranties">
+            <div className="active_userwarranty">Active Warranties</div>
+            <div className="active_userwarranty">Pending Warranties</div>
+            <div className="active_userwarranty">Expired Warranties</div>
+          </div>
         </div>
-        <div className='buyermain'></div>
+        <div className="buyermain">
+          <div className="buyermain_cards">
+            <CardBuyerdashboard />
+            <CardBuyerdashboard />
+            <CardBuyerdashboard />
+            <CardBuyerdashboard />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default BuyerDashboard;
