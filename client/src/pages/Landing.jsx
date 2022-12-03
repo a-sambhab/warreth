f6788cd0f90e3e2923c79cce8bcfe42c9df55cfc
kumarshantanu01 <shantanuk280@gmail.com>
2022-12-03 10:57:00 +0530

    minor

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import * as PushApi from '@pushprotocol/restapi';
-import './Landing.css';
+import React from "react";
+import * as PushApi from "@pushprotocol/restapi";
+import "./Landing.css";
 
-const fetchNotifs = async() => {
+const fetchNotifs = async () => {
   const notifications = await PushApi.user.getFeeds({
-      user: 'eip155:42:0xD8634C39BBFd4033c0d3289C4515275102423681', // user address in CAIP
-      env: 'staging'
+    user: "eip155:42:0xD8634C39BBFd4033c0d3289C4515275102423681", // user address in CAIP
+    env: "staging",
   });
 
-  console.log('Notifications: \n\n', notifications);
-}
+  console.log("Notifications: \n\n", notifications);
+};
 
 const Landing = () => {
   // fetchNotifs();
   return (
-    <div className='landingcontain'>Landing</div>
-  )
-}
+    <>
+      <div className="landingpage">
+        <div className="landingpage_left"></div>
+        <div className="landingpage_right"></div>
+      </div>
+      <div className="landingcontain">Landing</div>
+    </>
+  );
+};
 
-export default Landing
+export default Landing;
