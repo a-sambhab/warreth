0ee1a9ab6a609e9833bad798e9b6b8d25e1fd96d
a-sambhab <saisambhav21chaini@gmail.com>
2022-12-03 11:11:17 +0530

    Minor

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import './Navbar.css';
 
 const Navbar = () => {
   const [connected, setConnected] = useState(false);
+  const connectwallet = () => {
+    setConnected(true);
+  }
   return (
     <div className='navcontainer'>
       <div className='navlogo'>
@@ -10,14 +13,16 @@ const Navbar = () => {
       </div>
       {connected?
         <div className='connected'>
-          <img src='https://res.cloudinary.com/dgy8ybeoy/image/upload/v1670044645/Group_mr6k1t.png'/>
+          <img src='https://res.cloudinary.com/dgy8ybeoy/image/upload/v1670044645/Group_mr6k1t.png' className='navnotif'/>
           <div className='navaddress'>
             Hey, 0x84151...19655
           </div>
         </div>
       :
         <div className='connectwallet'>
-          Connect Wallet
+          <button className='connectbutton' onClick={connectwallet}>
+            Connect Wallet
+          </button>
         </div>
       }
     </div>
