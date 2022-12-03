804e6e09a623e98ea9af271c06ce060696555a04
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-12-03 16:41:22 +0530

    seller dashboard update

--- client/src/pages/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
+import React from "react";
 import "./sellerdashboard.css";
 import { CardBuyerdashboard } from "../components/Card_buyerdashboard";
+import { NavLink } from "react-router-dom";
 const SellerDashboard = () => {
   return (
     <>
-         <div className="buyercontain">
-      <div className="buyerinnercontain">
-        <div className="buyersidebar">
-          <div className="buyersidebar_warranties">
-            <div className="active_userwarranty">Active Warranties</div>
-            <div className="active_userwarranty">Pending Warranties</div>
-            <div className="active_userwarranty">Expired Warranties</div>
+      <div className="buyercontain">
+        <div className="buyerinnercontain">
+          <div className="buyersidebar">
+            <div className="buyersidebar_warranties">
+              <div className="active_userwarranty">Active Warranties</div>
+              <div className="active_userwarranty">Pending Warranties</div>
+              <div className="active_userwarranty">Expired Warranties</div>
+            </div>
+            <NavLink className="seller_button">Create Warranty</NavLink>
           </div>
-          <div className='seller_button'>Create Warranty</div>
-        </div>
-        <div className="buyermain">
-          <div className="buyermain_cards">
-            <CardBuyerdashboard />
-            <CardBuyerdashboard />
-            <CardBuyerdashboard />
-            <CardBuyerdashboard />
+          <div className="buyermain">
+            <div className="buyermain_cards">
+              <CardBuyerdashboard />
+              <CardBuyerdashboard />
+              <CardBuyerdashboard />
+              <CardBuyerdashboard />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default SellerDashboard
+export default SellerDashboard;
