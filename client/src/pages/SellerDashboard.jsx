014290726d092f8c5c8fb9705f001c6a9aa4cb95
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-12-03 18:25:52 +0530

    minor

--- client/src/pages/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import "./sellerdashboard.css";
 import { CardBuyerdashboard } from "../components/Card_buyerdashboard";
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import { NavLink } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import Web3Context from "../contexts";
 import { getSellerNFTs } from "../contexts/useContract/readContract";
 // >>>>>>> c631359b3abad97fd3ff8a58ee44176f26e1bcb1
-=======
-import { NavLink, useParams } from 'react-router-dom';
-import Web3Context from '../contexts';
-import { getSellerNFTs } from '../contexts/useContract/readContract';
->>>>>>> 8bb6a7d21725cd1bc9b5cefa05c15861eb5ed5df
+// =======
+// >>>>>>> 8bb6a7d21725cd1bc9b5cefa05c15861eb5ed5df
 const SellerDashboard = () => {
-  const {add} = useParams();
-  const {connectWallet, account, Contract, sellerI} = useContext(Web3Context);
+  const { add } = useParams();
+  const { connectWallet, account, Contract, sellerI } = useContext(Web3Context);
   const [nfts, setNfts] = useState([]);
-  const [status, setStatus] = useState(0)
+  const [status, setStatus] = useState(0);
   useEffect(() => {
     getData();
-   // console.log(nfts);
+    // console.log(nfts);
   }, [Contract, account, sellerI]);
   const getData = async () => {
     console.log(sellerI);
@@ -29,38 +26,68 @@ const SellerDashboard = () => {
   };
   return (
     <>
-         <div className="buyercontain">
-      <div className="buyerinnercontain">
-        <div className="buyersidebar">
-          <div className="buyersidebar_warranties">
-            <div className="active_userwarranty" onClick={()=>{setStatus(2)}}>Active Warranties</div>
-            <div className="active_userwarranty" onClick={()=>{setStatus(0)}}>Pending Warranties</div>
-            <div className="active_userwarranty" onClick={()=>{setStatus(3)}}>Expired Warranties</div>
+      <div className="buyercontain">
+        <div className="buyerinnercontain">
+          <div className="buyersidebar">
+            <div className="buyersidebar_warranties">
+              <div
+                className="active_userwarranty"
+                onClick={() => {
+                  setStatus(2);
+                }}
+              >
+                Active Warranties
+              </div>
+              <div
+                className="active_userwarranty"
+                onClick={() => {
+                  setStatus(0);
+                }}
+              >
+                Pending Warranties
+              </div>
+              <div
+                className="active_userwarranty"
+                onClick={() => {
+                  setStatus(3);
+                }}
+              >
+                Expired Warranties
+              </div>
+            </div>
+            <NavLink to={`/createwarranty/${add}`} className="seller_button">
+              Create Warranty
+            </NavLink>
           </div>
-          <NavLink to={`/createwarranty/${add}`} className='seller_button'>Create Warranty</NavLink>
-        </div>
-        <div className="buyermain">
-          <div className="buyermain_cards">
-            {
-              nfts.length && nfts
-                .filter((res)=>res.status==status)
-                .map((obj)=>{
-                  const {expiry, status, creationTime, productId, buyers, imageURI, tokenId} = obj;
-                  return(
-                    <CardBuyerdashboard
-                    expiry = {expiry}
-                    creationTime = {creationTime}
-                    productId = {productId}
-                    buyers = {buyers}
-                    imageURI = {imageURI}
-                    tokenId = {tokenId}
-                  />
-                  )
-                })
-            }
+          <div className="buyermain">
+            <div className="buyermain_cards">
+              {nfts.length &&
+                nfts
+                  .filter((res) => res.status == status)
+                  .map((obj) => {
+                    const {
+                      expiry,
+                      status,
+                      creationTime,
+                      productId,
+                      buyers,
+                      imageURI,
+                      tokenId,
+                    } = obj;
+                    return (
+                      <CardBuyerdashboard
+                        expiry={expiry}
+                        creationTime={creationTime}
+                        productId={productId}
+                        buyers={buyers}
+                        imageURI={imageURI}
+                        tokenId={tokenId}
+                      />
+                    );
+                  })}
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
