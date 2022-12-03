cc490dd1d75023da5f0fe38995c0dd5de978d6b6
a-sambhab <saisambhav21chaini@gmail.com>
2022-12-03 10:37:10 +0530

    Added Push Test

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import * as PushApi from '@pushprotocol/restapi';
+import './Landing.css';
+
+const fetchNotifs = async() => {
+  const notifications = await PushApi.user.getFeeds({
+      user: 'eip155:42:0xD8634C39BBFd4033c0d3289C4515275102423681', // user address in CAIP
+      env: 'staging'
+  });
+
+  console.log('Notifications: \n\n', notifications);
+}
 
 const Landing = () => {
+  // fetchNotifs();
   return (
-    <div>Landing</div>
+    <div className='landingcontain'>Landing</div>
   )
 }
 
